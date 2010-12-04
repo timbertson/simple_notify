971ec9c6baff7612bb010e482a93add7ce4c7747
gfxmonk <tim3d.junk@gmail.com>
2010-12-04 21:55:23 +1100

    I really don't like setuptools...

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+## NOTE: ##
+## setup.py is not maintained, and is only provided for convenience.
+## please see http://gfxmonk.net/dist/0install/index/ for
+## up-to-date installable packages.
+
+from setuptools import *
+setup(
+	name='simple_notify',
+	version='0.1',
+	author_email='tim@gfxmonk.net',
+	author='Tim Cuthbertson',
+	url='http://gfxmonk.net/dist/0install/simple_notify.xml',
+	description="simple inotify wrapper",
+	long_description="simple inotify wrapper",
+	py_modules = ['simple_notify'],
+	license='BSD',
+)
