d87f83f2e42e64c650985b7b1a9cc11d7df38acf
gfxmonk <tim3d.junk@gmail.com>
2010-12-04 20:44:57 +1100

    initial

---
+++ simple_inotify_test.py
@@ -0,0 +1,161 @@
+import unittest
+import tempfile
+import shutil
+import os
+import time
+
+import simple_notify
+
+class Monitor():
+	def __init__(self, base, **kw):
+		self._events = []
+		self.base = base
+		self.watcher = simple_notify.watch(base, callback=self.on_change, **kw)
+	
+	def _sleep(self):
+		time.sleep(0.1)
+
+	def clear(self):
+		self._sleep()
+		self._events = []
+	
+	@property
+	def events(self):
+		self._sleep()
+		return self._events
+	
+	@property
+	def changed_paths(self):
+		return list(sorted(set(map(lambda x: x.path, self.events))))
+
+	def on_change(self, event):
+		self._events.append(event)
+	
+	def stop(self):
+		self.watcher.stop()
+
+
+class BaseTest(unittest.TestCase):
+	def setUp(self):
+		self.base = tempfile.mkdtemp()
+	
+	def tearDown(self):
+		self.monitor.stop()
+		shutil.rmtree(self.base)
+
+	def touch(self, path):
+		with open(self.fullpath(path), 'w') as f:
+			f.write("")
+
+	def mkdir(self, path):
+		os.mkdir(self.fullpath(path))
+	
+	def rm(self, path):
+		path = self.fullpath(path)
+		if os.path.isdir(path):
+			shutil.rmtree(path)
+		else:
+			os.remove(path)
+	
+	def fullpath(self, path):
+		return os.path.join(self.base, path)
+
+	def mv(self, src, dest):
+		src, dest = map(self.fullpath, (src, dest))
+		os.rename(src, dest)
+	
+class ModificationTest(BaseTest):
+	def setUp(self):
+		super(type(self), self).setUp()
+		self.monitor = Monitor(self.base)
+	
+	def test_should_detect_a_new_file(self):
+		self.touch("some_file")
+		self.assertEquals(self.monitor.changed_paths, ["some_file"])
+	
+	def test_should_detect_a_modified_file(self):
+		self.touch("some_file")
+		self.monitor.clear()
+
+		self.touch("some_file")
+		self.assertEquals(self.monitor.changed_paths, ["some_file"])
+	
+	def test_should_detect_a_deleted_file(self):
+		self.touch("f")
+		self.monitor.clear()
+		self.rm("f")
+		
+		self.assertEquals(self.monitor.events, [simple_notify.Event(base="", event=simple_notify.Event.REMOVED, exists=False, name="f", is_dir=False)])
+
+	def test_should_detect_a_deleted_dir(self):
+		self.mkdir("d")
+		self.monitor.clear()
+		self.rm("d")
+		
+		self.assertEquals(self.monitor.events, [simple_notify.Event(base="", event=simple_notify.Event.REMOVED, exists=False, name="d", is_dir=True)])
+
+	def test_should_detect_a_file_move(self):
+		self.touch("a")
+		self.monitor.clear()
+		self.mv("a", "b")
+		
+		self.assertEquals(self.monitor.events, [
+			simple_notify.Event(base="", event=simple_notify.Event.MOVED_FROM, exists=False, name="a", is_dir=True),
+			simple_notify.Event(base="", event=simple_notify.Event.MOVED_TO, exists=True, name="b", is_dir=True)
+			])
+
+class NestedTest(BaseTest):
+	def setUp(self):
+		super(type(self), self).setUp()
+		self.mkdir("nested")
+		self.monitor = Monitor(self.base, ignore_modifications=False)
+
+	def test_should_detect_events_to_a_nested_dir(self):
+		self.mkdir("nested/dir")
+		self.assertEquals(self.monitor.events, [simple_notify.Event(base="nested", event=simple_notify.Event.ADDED, exists=True, name="dir", is_dir=True)])
+
+	def test_should_detect_events_to_a_nested_file(self):
+		self.touch("nested/file")
+		try:
+			# this probably shouldn't be the case, but is acceptable
+			self.assertEquals(self.monitor.events, [
+				simple_notify.Event(base="nested", event=simple_notify.Event.ADDED, exists=True, name="file", is_dir=False),
+				simple_notify.Event(base="nested", event=simple_notify.Event.MODIFIED, exists=True, name="file", is_dir=False)
+			])
+		except AssertionError:
+			# this is ideal:
+			self.assertEquals(self.monitor.events, [simple_notify.Event(base="nested", event=simple_notify.Event.ADDED, exists=True, name="file", is_dir=False)])
+
+	def test_should_detect_a_file_move_between_directories(self):
+		self.mkdir("dest")
+		self.touch("nested/file")
+		self.monitor.clear()
+
+		self.mv("nested/file", "dest/file2")
+		self.assertEquals(self.monitor.events, [
+			simple_notify.Event(base="nested", event=simple_notify.Event.MOVED_FROM, exists=False, name="file", is_dir=False),
+			simple_notify.Event(base="dest", event=simple_notify.Event.MOVED_TO, exists=True, name="file2", is_dir=False),
+		])
+	
+	def test_should_only_mention_directories_even_if_children__move(self):
+		self.touch("nested/file")
+		self.monitor.clear()
+
+		self.mv("nested", "dest")
+		self.assertEquals(self.monitor.events, [
+			simple_notify.Event(base="", event=simple_notify.Event.MOVED_FROM, exists=False, name="nested", is_dir=True),
+			simple_notify.Event(base="", event=simple_notify.Event.MOVED_TO, exists=True, name="dest", is_dir=True),
+		])
+
+class IgnoreNodificationTest(BaseTest):
+	def setUp(self):
+		super(type(self), self).setUp()
+		self.monitor = Monitor(self.base, ignore_modifications=True)
+	
+	def test_should_ignore_a_modified_file(self):
+		self.touch("some_file")
+
+		self.monitor.clear()
+		self.touch("some_file")
+		self.assertEquals(self.monitor.changed_paths, [])
+
---
+++ simple_notify.py
@@ -0,0 +1,100 @@
+import logging
+import Queue as queue
+import os
+
+from pyinotify import WatchManager, ThreadedNotifier, ProcessEvent
+from pyinotify import IN_DELETE, IN_CREATE, IN_MOVED_FROM, IN_MOVED_TO, IN_CLOSE_WRITE
+EVENT_MASK = IN_DELETE | IN_CREATE | IN_MOVED_TO | IN_MOVED_FROM
+EVENT_MASK_WITH_MODIFICATIONS = EVENT_MASK | IN_CLOSE_WRITE
+
+logger = logging.getLogger(__name__)
+class Watch(object):
+	def __init__(self, path, callback, recursive=True, ignore_modifications=False):
+		self.event_mask = EVENT_MASK if ignore_modifications else EVENT_MASK_WITH_MODIFICATIONS
+		self._dir_queue = queue.Queue()
+		self._root = os.path.realpath(path)
+
+		self._watch_manager = WatchManager()
+
+		self._processor = FileProcessEvent(directory_queue=self._dir_queue, root=self._root, callback=callback)
+		self._notifier = ThreadedNotifier(self._watch_manager, self._processor)
+
+		self._notifier.name = "[inotify] notifier"
+		self._notifier.daemon = True
+		self._notifier.start()
+
+		self._watch_manager.add_watch(path, self.event_mask, rec=True, auto_add=True)
+	
+	def stop(self):
+		self._notifier.stop()
+
+class Event(object):
+	MOVED_TO = 'MOVED_TO'
+	MOVED_FROM = 'MOVED_FROM'
+	MODIFIED = 'MODIFIED'
+	ADDED = 'ADDED'
+	REMOVED = 'REMOVED'
+
+	def __init__(self, base, event, exists, name, is_dir):
+		self.base = base
+		self.name = name
+		self.event = event
+		self.exists = exists
+		self.is_dir=is_dir
+	
+	def __eq__(self, other):
+		if not isinstance(other, type(self)): return False
+		attrs = ['base', 'name', 'event', 'exists']
+		return map(lambda a: getattr(self, a) , attrs) == \
+		       map(lambda a: getattr(other, a), attrs)
+	
+	def __repr__(self):
+		attrs = ['base', 'name', 'event', 'exists']
+		return repr(map(lambda a: getattr(self, a) , attrs))
+		return "<# %s %s at %s >"% (
+				self.event, "dir" if self.is_dir else "file", self.path)
+	
+	__str__ = __repr__
+	
+	@property
+	def path(self):
+		return os.path.join(self.base, self.name or '')
+
+def handler(which, exists):
+	def handle_event(self, event):
+		event_path = self.relative_path(event.path)
+		path = os.path.join(event_path, event.name)
+		logging.debug("event %s occurred to path %s" % (which, path))
+		is_dir = self.is_dir(event)
+		#if is_dir:
+		#	self._dir_queue.put((path, exists))
+		#else:
+		self.on_change(Event(base=event_path, name=event.name, event=which, exists=exists, is_dir=is_dir))
+	return handle_event
+
+class FileProcessEvent(ProcessEvent):
+	def __init__(self, directory_queue, root, callback):
+		self._dir_queue = directory_queue
+		self._root = root
+		self.on_change = callback
+	
+	def is_dir(self, event):
+		if hasattr(event, "dir"):
+			return event.dir
+		else:
+			return event.is_dir
+	
+	def relative_path(self, path):
+		if path.startswith(self._root):
+			return path[len(self._root):].lstrip(os.path.sep)
+		else:
+			logger.warn("non-relative path encountered: %s" % (path,))
+		return path
+
+	process_IN_CLOSE_WRITE = handler(Event.MODIFIED, True)
+	process_IN_CREATE = handler(Event.ADDED, True)
+	process_IN_DELETE = handler(Event.REMOVED, False)
+	process_IN_MOVED_FROM = handler(Event.MOVED_FROM, False)
+	process_IN_MOVED_TO = handler(Event.MOVED_TO, True)
+
+watch = Watch
